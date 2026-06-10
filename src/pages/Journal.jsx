8babd112fe9218907ea365df05bab5c75c9734de
yemi <o.ogunbadejo7@gmail.com>
2026-06-10 15:50:20 +0100

    feat: added a conditional for a location with no enemies in the filter

--- src/pages/Journal.jsx
+++ src/pages/Journal.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { useNavigate } from 'react-router-dom';
+
 
 function Journal() {
     const navigate = useNavigate();
@@ -10,11 +13,14 @@ function Journal() {
     const [enemies, setEnemies] = useState([]);
     const [seletedEnemy, setSelectedEnemy] = useState(null);
     const [seletedEnemyId, setSelectedEnemyId] = useState(null);
-    const [fetching, setFetching] = useState(true)
-    const [showDeleteConfirm, setShowDeleteConfirm] = useState(false)
+    const [fetching, setFetching] = useState(true);
+    const [showDeleteConfirm, setShowDeleteConfirm] = useState(false);
+    const [locations, setLocations] = useState([]);
+    const [selectedLocationId, setSelectedLocationId] = useState('');
 
     useEffect(() => {
         getEnemies();
+        getLocations();
     }, [])
 
     const getEnemies = async () => {
@@ -48,8 +54,6 @@ function Journal() {
 
     const deleteSingleEnemy = async () => {
         try {
-            console.log(seletedEnemy);
-            console.log(seletedEnemyId);
             if (!seletedEnemy) return;
             const response = await axios.delete(`${import.meta.env.VITE_SERVER_URL}/enemies/${seletedEnemyId}`)
             setSelectedEnemy(null);
@@ -59,30 +63,79 @@ function Journal() {
         }
     }
 
+    const getLocations = async () => {
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/locations`)
+        setLocations(response.data);
+    }
+
+    useEffect(() => {
+        if (selectedLocationId === '') getEnemies();
+        filterEnemiesByLocation();
+    }, [selectedLocationId])
+
+    const filterEnemiesByLocation = async () => {
+        if (!selectedLocationId) return;
+
+        try {
+            const locationRequest = await axios.get(`${import.meta.env.VITE_SERVER_URL}/locations/${selectedLocationId}`)
+            const filteredEnemyIds = locationRequest.data.enemyIds;
+            const enemyRequest = filteredEnemyIds.map((enemyId) => {
+                return axios.get(`${import.meta.env.VITE_SERVER_URL}/enemies/${enemyId}`);
+            })
+            const enemyResponse = await Promise.all(enemyRequest);
+            const enemyData = enemyResponse.map((res) => res.data)
+            console.log(enemyData);
+            setEnemies(enemyData);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     if (!enemies || fetching) return <h1 className='mt-5 p-5'>Loading...</h1>
 
     return (
         <div className="journal">
-            <div className="filter-select">
-                <ButtonGroup aria-label="enemy filter">
-                    <Button className=''>Location 1</Button>
-                    <Button className=''>Location 2</Button>
-                    <Button className=''>Location 3</Button>
-                </ButtonGroup>
-            </div>
+            <Form data-bs-theme="dark" className='filter-select'>
+                <FloatingLabel
+                    controlId='floatingSelect'
+                    label="Choose a Location">
+                    <Form.Select
+                        value={selectedLocationId}
+                        onChange={(e) => { setSelectedLocationId(e.target.value) }}
+                    >
+                        <option value="">All Locations</option>
+                        {
+                            locations.map((location) => {
+                                return (
+                                    <option
+                                        key={location.id}
+                                        value={location.id}
+                                    >
+                                        {location.location}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Form.Select>
+                </FloatingLabel>
+            </Form>
             <div className='enemy-grid'>
                 <div className='enemy-list'>
-                    {enemies.map((enemy) => {
-                        return (
-                            <div
-                                className='enemy-item'
-                                key={enemy.id}
-                                onClick={() => setSelectedEnemyId(enemy.id)}
-                            >
-                                <p>{enemy.name}</p>
-                            </div>
-                        )
-                    })}
+                    {
+                        enemies.length === 0 ?
+                            <div className='enemy-item'><p>no enemies</p></div> :
+                            enemies.map((enemy) => {
+
+                                return (
+                                    <div
+                                        className='enemy-item'
+                                        key={enemy.id}
+                                        onClick={() => setSelectedEnemyId(enemy.id)}
+                                    >
+                                        <p>{enemy.name}</p>
+                                    </div>
+                                )
+                            })}
                 </div>
                 <div className='enemy-picture-box'>
                     {seletedEnemy ? <img className='enemy-image' src={seletedEnemy.images} /> : 'image'}
